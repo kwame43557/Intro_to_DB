b8cd97ca74ceff8f006b421bd5d1063b92ad90c5
Kwame Takyi <takyiemmanuelkwame@gmail.com>
2025-08-30 17:58:50 +0000

    Made few corrections on task_4 file

--- task_4.sql
+++ task_4.sql
@@ -2,8 +2,7 @@ USE alx_book_store;
 
 
 SELECT COLUMN_NAME AS 'Column Name',
-       DATA_TYPE AS 'Data Type',
-       CHARACTER_MAXIMUM_LENGTH AS 'Max Length',
+       COLUMN_TYPE AS 'Column Type',
        IS_NULLABLE AS 'Nullable',
        COLUMN_DEFAULT AS 'Default Value'
 FROM INFORMATION_SCHEMA.COLUMNS
