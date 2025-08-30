0a02de57b85ff114cf3ad3cc22dc65ca76330b8d
Kwame Takyi <takyiemmanuelkwame@gmail.com>
2025-08-30 17:56:09 +0000

    Added task_4 sql file

---
+++ task_4.sql
@@ -0,0 +1,10 @@
+USE alx_book_store;
+
+
+SELECT COLUMN_NAME AS 'Column Name',
+       DATA_TYPE AS 'Data Type',
+       CHARACTER_MAXIMUM_LENGTH AS 'Max Length',
+       IS_NULLABLE AS 'Nullable',
+       COLUMN_DEFAULT AS 'Default Value'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store';
