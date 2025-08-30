77db0014287772a1b41d02bc8f7876f8e664203b
Kwame Takyi <takyiemmanuelkwame@gmail.com>
2025-08-30 18:28:51 +0000

    Made few corrections in task 5

--- task_5.sql
+++ task_5.sql
@@ -2,4 +2,5 @@ USE alx_book_store;
 
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'),
+(2, 'Peter John', 'pjohn@sandtech.com, '124 Joy Ave.');
